653125b8649896cf5fb2aabac595aca89502f4de
Kim <kim.tam@pcsfg.com.hk>
2022-08-08 15:22:21 +0800

    Initial commit

---
+++ PCKit/src/main/java/com/pcsfg/pckit/chatkit/view/ChatTextView.kt
@@ -0,0 +1,11 @@
+package com.pcsfg.pckit.chatkit.view
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun ChatTextView()
+{
+    Text(text = "Chat View Text123")
+}
